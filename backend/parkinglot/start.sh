11f8074f81400d221b35e5b8af7e52dc134bbd18
ppasterz <ppasterz@microstrategy.com>
2018-07-16 23:19:47 +0200

    Add actuator

--- backend/parkinglot/start.sh
+++ backend/parkinglot/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo Some clean up
+echo ++++Start MySql Docker++++
+docker run --name demo-mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=parking -p 3306:3306 -d mysql:5.7
+
+echo ++++Some clean up++++
 mvn clean
 
-echo Create Database
-mysql -h localhost -u root -e "create database parking;"
-
-echo Run application
+echo ++++Run application++++
 mvn spring-boot:run
