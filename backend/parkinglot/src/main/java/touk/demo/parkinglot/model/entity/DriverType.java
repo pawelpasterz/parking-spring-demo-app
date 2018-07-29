864f28a4e617510d971124702ed5f42ddab4331e
ppasterz <ppasterz@microstrategy.com>
2018-07-29 16:50:04 +0200

    Refactor driver type

--- backend/parkinglot/src/main/java/touk/demo/parkinglot/model/entity/DriverType.java
+++ backend/parkinglot/src/main/java/touk/demo/parkinglot/model/entity/DriverType.java
@@ -3,7 +3,11 @@ package touk.demo.parkinglot.model.entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@AllArgsConstructor
+@Data
 @Entity
 @Table(name = "driver_type")
 public final class DriverType {
@@ -14,60 +18,4 @@ public final class DriverType {
   private int firstHour;
   private int secondHour;
   private double thirdHour;
-
-  public DriverType() {}
-
-  public DriverType(String roleName, int firstHour, int secondHour, double thirdHour) {
-    this.roleName = roleName;
-    this.firstHour = firstHour;
-    this.secondHour = secondHour;
-    this.thirdHour = thirdHour;
-  }
-
-  public String getRoleName() {
-    return roleName;
-  }
-
-  public void setRoleName(String roleName) {
-    this.roleName = roleName;
-  }
-
-  public int getFirstHour() {
-    return firstHour;
-  }
-
-  public void setFirstHour(int firstHour) {
-    this.firstHour = firstHour;
-  }
-
-  public int getSecondHour() {
-    return secondHour;
-  }
-
-  public void setSecondHour(int secondHour) {
-    this.secondHour = secondHour;
-  }
-
-  public double getThirdHour() {
-    return thirdHour;
-  }
-
-  public void setThirdHour(double thirdHour) {
-    this.thirdHour = thirdHour;
-  }
-
-  @Override
-  public String toString() {
-    return "DriverType{"
-        + "roleName='"
-        + roleName
-        + '\''
-        + ", firstHour="
-        + firstHour
-        + ", secondHour="
-        + secondHour
-        + ", thirdHour="
-        + thirdHour
-        + '}';
-  }
 }
